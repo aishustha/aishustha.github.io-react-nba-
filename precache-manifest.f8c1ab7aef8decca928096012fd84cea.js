0f0a704e0636632db63ff7dd175a18d5f604a4b4
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2020-11-11 14:44:56 +0545

    added project

---
+++ precache-manifest.f8c1ab7aef8decca928096012fd84cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd6cb9eb35ec29aa8afc63b096b0be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd988e9bf19d535c777",
+    "url": "/static/css/main.571b9dae.chunk.css"
+  },
+  {
+    "revision": "1a85d77cb8a12899c781",
+    "url": "/static/js/2.b0e76187.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b0e76187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd988e9bf19d535c777",
+    "url": "/static/js/main.8ce6c3a7.chunk.js"
+  },
+  {
+    "revision": "78c7502d558f31c3b312",
+    "url": "/static/js/runtime-main.4b514629.js"
+  }
+]);
